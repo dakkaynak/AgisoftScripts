ce2a63b3a3361928d21443b42e4c2cde43ba4123
dakkaynak <derya.akkaynak@gmail.com>
2020-10-08 09:24:31 -0400

    Adding check for aligned cameras
    Also check for whether absolute scale is entered

--- exportDepth_v4.py
+++ exportDepth_v4.py
@@ -1,18 +1,29 @@
 # This script exports a SCALED DENSE depth map, after MESH stage has been completed, and a scale has been entered.
+# If no scale is entered, depth maps have relative scale.
+# Exported depth maps are the same size as the original images in the chunk, but compressed. This level of compression does not affect the accuracy needed for color correciton.
+# Derya Akkaynak
 
 import Metashape
 
+# Select active chunk
+chunk = Metashape.app.document.chunk 
 
-chunk = Metashape.app.document.chunk # active chunk
-scale = chunk.transform.scale
+# Is the model scaled? If not assign scale of 1
+if chunk.transform.scale is None:
+    scale = 1
+else:
+	scale = chunk.transform.scale
+		
+# Export depth maps for all aligned cameras in chunk		
 for camera in chunk.cameras:
-#camera = chunk.cameras[1]
-	depth = chunk.model.renderDepth(camera.transform, camera.sensor.calibration)
-	depth = depth * scale
-	depth = depth.convert(" ","F16")
-	#depth = depth.convert("RGB","F16")
-	compr = Metashape.ImageCompression()
-	compr.tiff_compression = Metashape.ImageCompression().TiffCompressionDeflate
-	depth.save("/Users/deryaakkaynak/Desktop/depth" + camera.label + ".tif", compression = compr)
+	# First check if the camera is aligned
+	if camera.transform:
+		depth = chunk.model.renderDepth(camera.transform, camera.sensor.calibration)
+		depth = depth * scale
+		depth = depth.convert(" ","F16")
+		compr = Metashape.ImageCompression()
+		compr.tiff_compression = Metashape.ImageCompression().TiffCompressionDeflate
+		# Remember to update this path according to your folders
+		depth.save("/Users/deryaakkaynak/Desktop/depth" + camera.label + ".tif", compression = compr)
 
 						
